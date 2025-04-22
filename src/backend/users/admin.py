6216a6a49bd29936f67ec709086909a96ad1304d
어영민 <eoyeongmin@Yms-MacBook-Pro.local>
2025-04-22 11:56:55 +0900

    feat: 관리자 페이지 설정

--- src/backend/users/admin.py
+++ src/backend/users/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from django.contrib import admin
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+@admin.register(User)
+class CustomUserAdmin(admin.ModelAdmin):
+    list_display = ['id', 'username', 'email', 'is_staff', 'is_active']
+    search_fields = ['username', 'email']
+
